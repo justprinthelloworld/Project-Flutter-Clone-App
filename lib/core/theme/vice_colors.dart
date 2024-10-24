e8c33abbcc139add246bb2741084ef42dc33a9b9
justprinthelloworld <justprinthelloworld@gmail.com>
2024-10-24 16:12:52 +0700

    Initial commit

---
+++ lib/core/theme/vice_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ViceColors {
+  const ViceColors._();
+
+  static const purple = Colors.purpleAccent;
+  static const lightGreen = Colors.greenAccent;
+  static const textColor = Colors.black;
+  static const List<Color> scaffoldColors = [
+    Color.fromARGB(255, 255, 51, 51),
+    Color.fromARGB(255, 252, 215, 5),
+  ];
+}
